987ec3dfdd9593214ec77c3d105ea5c8935955a7
RituSingh2002 <ritusingh29g@gmail.com>
2022-06-04 03:27:06 +0530

    complete reset password and verify email by the help of nodemailer

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,24 @@
-const express =require('express');
-const app=express();
-const router=require('./routes/user.js');
- app.use(router);
-// MVC => Model view controller
-app.get('/about',(req,res)=>{
-    res.send("<h1> hello backend server has created for ABOUT</h1>");
-    });
-app.listen(8000,(req,res)=>{
-    console.log('the port is listening on port 8000');
-})
- 
+const express = require("express");
+require("./db");
+const userRouter = require("./routes/user");
+
+const app = express();
+app.use(express.json());
+app.use("/api/user", userRouter);
+
+app.post(
+  "/sign-in",
+  (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.json({ error: "email/password missing!" });
+    next();
+  },
+  (req, res) => {
+    res.send("<h1>Hello I am about page</h1>");
+  }
+);
+
+app.listen(8000, () => {
+  console.log("the port is listening on port 8000");
+});
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,36 @@
-const express=require('express');
-const router=express.Router();
-router.get('/',(req,res)=>{
-    res.send("<h1> hello backend server has created</h1>");
-    });
-    module.exports=router;
+const express = require("express");
+const {
+  create,
+  verifyEmail,
+  resendEmailVerificationToken,
+  forgetPassword,
+  sendResetPasswordTokenStatus,
+  resetPassword,
+} = require("../controllers/user");
+const { isValidPassResetToken } = require("../middlewares/user");
+const {
+  userValidtor,
+  validate,
+  validatePassword,
+} = require("../middlewares/validator");
+
+const router = express.Router();
+
+router.post("/create", userValidtor, validate, create);
+router.post("/verify-email", verifyEmail);
+router.post("/resend-email-verification-token", resendEmailVerificationToken);
+router.post("/forget-password", forgetPassword);
+router.post(
+  "/verify-pass-reset-token",
+  isValidPassResetToken,
+  sendResetPasswordTokenStatus
+);
+router.post(
+  "/reset-password",
+  validatePassword,
+  validate,
+  isValidPassResetToken,
+  resetPassword
+);
+
+module.exports = router;
